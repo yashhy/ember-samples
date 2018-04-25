610722aefd0cd0766d639deedd9394afff0726cc
Yashwanth Harikrishnan <yashwanth.harikrishnan@freshworks.com>
2018-04-25 13:19:50 +0530

    - included ember bootstrap - more styles! more fun!

--- app/routes/rentals.js
+++ app/routes/rentals.js
@@ -1,4 +1,22 @@
 import Route from '@ember/routing/route';
 
 export default Route.extend({
+  model() {
+    return[{
+      name: 'Ascendas',
+      location: 'Taramani',
+      city: 'Chennai',
+      id: 1
+    }, {
+      name: 'RMZ',
+      location: 'MGR Salai',
+      city: 'Chennai',
+      id: 2
+    }, {
+      name: 'SP Info City',
+      location: 'MGR Salai',
+      city: 'Chennai',
+      id: 3
+    }]
+  }
 });
